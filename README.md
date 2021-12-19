38245dc90e57ec2158e3601893e4f4e175c19012
pheithar <pheithar@hotmail.com>
2021-12-20 00:41:56 +0100

    README and comentaries

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Mask_RCNN_Web
+# COCO SDD web application
+
+A small web application for object recognition over image and video.
+
+![prediction](assets/images/prediction1.png)
+
+The application is available at [this webpage](https://pheithar.github.io/coco_ssd_web/).
+
+The webpage uses the [COCO SSD model](https://github.com/tensorflow/tfjs-models/tree/master/coco-ssd) with the [tensorflow js framework](https://www.tensorflow.org/js?hl=en-419) to create a small object detection application. In this application there are 5 images and 2 videos to preview, but you can upload your own images or videos to try it out. You can use your webcam too.
+
+The COCO SSD is a pre-trained model over 90 different classes, and can identify multiple objects in a single image. I used the *TensorFlow.js* version of it, but the original paper is available through [this link](https://arxiv.org/abs/1512.02325).
+
+This small project is focused on the deployment of the network, rather than in doing further training, and can be seen as a testing tool to get the accuracy of the model.
--- script.js
+++ script.js
@@ -1,23 +1,26 @@
+// All the labels that the model can predict. This is just used fr setting the colors
 const labels = ["unlabeled", "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "street sign", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "hat", "backpack", "umbrella", "shoe", "eye glasses", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "plate", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch", "potted plant", "bed", "mirror", "dining table", "window", "desk", "toilet", "door", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "blender", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"];
 
 const colors = [];
 
+// Each time, the set of colors are defined randomly and different for each class
 labels.forEach((label, i) => {
   var randomColor = Math.floor(Math.random()*16777215).toString(16);
   colors.push(randomColor);
 });
 
-$("#loaded").css("display", "none");
 
-var play_src = "assets/icons/play-button.png"
-var stop_src = "assets/icons/stop-button.png"
 
+// Define the model
 var model = null;
 
+// Sets the global variable 'model' to a value
 function setModel(model) {
   this.model = model;
 }
 
+// Functions for getting the direction of a image/video given the name
+// that is in HTNL
 function getImageByName(name) {
   return "assets/images/" + name + ".jpg"
 }
@@ -25,6 +28,11 @@ function getVideoByName(name) {
   return "assets/videos/" + name + ".mp4"
 }
 
+
+// Function for loading the model
+// Could have not use it as cocoSsd already has load, but allows debugging and
+// more control over the workflow. On the other hand, as it has an await, makes
+// the webpage unsuable (cannot click links nor do any action) while is loading
 async function loadModel() {
   console.log("Loading Model...");
   const model = await cocoSsd.load();
@@ -35,7 +43,7 @@ async function loadModel() {
   return model;
 }
 
-
+// then and catch of the loadModel()
 loadModel().then((model) => {
   // Show content of the web once the model is loaded
   $("#loaded").css("display", "block")
@@ -50,6 +58,7 @@ loadModel().then((model) => {
     drawImage();
   });
 
+  // Set the default selection and draw video for the default video
   $(".select-video").val('video1');
   $('.video').attr("src", getVideoByName("video1")).load(() => {
     drawVideo();
@@ -62,6 +71,7 @@ loadModel().then((model) => {
   return null;
 });
 
+// Function for drawing the image in the canvas
 function drawImage() {
   const img = $('.img')[0];
   const canvas = $('#canvas-image')[0];
@@ -74,6 +84,7 @@ function drawImage() {
   ctx.drawImage(img, 0, 0);
 }
 
+// Function for drawingthe prediction boxes in the canvas
 function predictImage() {
   const img = $('.img')[0];
   const canvas = $('#canvas-image')[0];
@@ -81,8 +92,9 @@ function predictImage() {
 
   ctx.font = "bold 12.5px Arial";
 
+  // Predict using cocoSsd
   model.detect(img).then((predictions) => {
-
+    // Loop through all the predictions
     predictions.forEach((prediction, i) => {
       const index = labels.indexOf(prediction.class);
       const color = "#" + colors[index];
@@ -106,17 +118,20 @@ function predictImage() {
   });
 }
 
-
+// Change the current image when the user change it
 $('.select-image').change(() => {
   $('.img').attr("src", getImageByName($('.select-image')[0].value)).load(() => {
     drawImage();
   });
 });
 
+// Call predictImage when the button 'predict' is clicked
 $(".predict-image-button").click(() => {
   predictImage();
 });
 
+// When clicked 'upload image', check if there is a file (it only accepts images)
+// and load it
 $(".upload-image-button").click(() => {
   const files = $(".input-image")[0].files
 
@@ -129,11 +144,17 @@ $(".upload-image-button").click(() => {
 });
 
 
+// Helping variables for checking if the video is in prediction mode or not,
+// and check if the webcam is on or not.
 var predict_video = false;
 var webcam = false;
 
+// Variables for the source for the play button
+var play_src = "assets/icons/play-button.png"
+var stop_src = "assets/icons/stop-button.png"
 
 
+// Same as drawImage, but with video. Draw video in the canvas
 function drawVideo() {
   const video = $('.video')[0];
   const canvas = $('#canvas-video')[0];
@@ -145,10 +166,14 @@ function drawVideo() {
 
   ctx.drawImage(video, 0, 0);
 
+  // If is in prediction mode, call the prediction
   if (predict_video) {
     predictVideo()
   }
 
+  // Control the image that is shown in the 'play button', and
+  // call the function for the next fram, as it needs to be reloaded
+  // if the video is playing
   if (!video.paused) {
     $(".play-stop-image").attr("src", stop_src)
     requestAnimationFrame(drawVideo);
@@ -159,6 +184,8 @@ function drawVideo() {
 
 }
 
+// Similar to predict image, but with video. Do the prediction of the video
+// using cocoSsd
 function predictVideo() {
   const video = $('.video')[0];
   const canvas = $('#canvas-video')[0];
@@ -192,16 +219,17 @@ function predictVideo() {
 }
 
 
-
+// Wait for the video to load before calling drawVideo
 $(".video").on("loadeddata", () => {
   drawVideo();
 });
 
-
+// When the play on the video ois call, draw the video in the canvas
 $(".video").on("play", () => {
   drawVideo();
 });
 
+// Change between play and stop when the button 'play/stop' is clicked
 $(".play-button").click(() => {
   const video = $(".video")[0];
   if (video.paused) {
@@ -212,6 +240,7 @@ $(".play-button").click(() => {
   }
 });
 
+// Change between different videos from the selector
 $('.select-video').change(() => {
   const src = $('.select-video')[0].value;
   $('.video').attr("src", getVideoByName(src)).load(() => {
@@ -219,6 +248,8 @@ $('.select-video').change(() => {
   });
 });
 
+// Activate/deactivate prediction mode when the button
+// 'prediction on/off' is clicked
 $(".predict-video-button").click(() => {
   predict_video = !predict_video;
 
@@ -232,6 +263,7 @@ $(".predict-video-button").click(() => {
   }
 });
 
+// Upload a video from the input. It only accepts video format
 $(".upload-video-button").click(() => {
   const files = $(".input-video")[0].files
 
@@ -243,6 +275,8 @@ $(".upload-video-button").click(() => {
   }
 });
 
+
+// If possible, activate the webcam of the user and use it as video
 $(".webcam-button").click(() => {
 
   if (!webcam && navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -253,6 +287,8 @@ $(".webcam-button").click(() => {
 
       webcam = !webcam;
     }).catch((err) => {
+      // Catch any possible error and show that there has been an
+      // error to the user
       console.log(err);
       $(".no-webcam").css("display", "block");
     });
@@ -263,12 +299,14 @@ $(".webcam-button").click(() => {
     const video = $(".video")[0]
     video.srcObject= null;
 
+    // Select default video on webcam off
     $(".select-video").val('video1');
     $('.video').attr("src", getVideoByName("video1")).load(() => {
       drawVideo();
     });
   }
   else {
+    // show that there has been an  error to the user
     $(".no-webcam").css("display", "block");
   }
 });
